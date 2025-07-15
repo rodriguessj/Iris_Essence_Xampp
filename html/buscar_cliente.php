d320d0c225b34fe1b2cc8b6a58bd691253407c0d
rodriguessj <roiz.rodr1gues23@gmail.com>
2025-07-15 15:33:24 -0300

    Update buscar_cliente.php

--- html/buscar_cliente.php
+++ html/buscar_cliente.php
@@ -23,7 +23,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && !empty($_POST['busca'])) {
     } else {
         $sql = "SELECT * FROM cliente WHERE nome LIKE :busca_nome ORDER BY nome ASC";
         $stmt = $pdo->prepare($sql);
-        $stmt->bindValue(':busca_nome', "%$busca%", PDO::PARAM_STR);
+        $stmt->bindValue(':busca_nome', "$busca%", PDO::PARAM_STR);
     }
 } else {
     $sql = "SELECT * FROM cliente ORDER BY nome ASC";

7fa658cd74eb76b01ffed2ceb74f65c8e691c399
rodriguessj <roiz.rodr1gues23@gmail.com>
2025-07-15 15:35:54 -0300

    Update buscar_procedimento.php

--- html/buscar_procedimento.php
+++ html/buscar_procedimento.php
@@ -22,7 +22,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && !empty($_POST['busca'])) {
     } else {
         $sql = "SELECT * FROM procedimento WHERE nome LIKE :busca_nome ORDER BY nome ASC";
         $stmt = $pdo->prepare($sql);
-        $stmt->bindValue(':busca_nome', "%$busca%", PDO::PARAM_STR);
+        $stmt->bindValue(':busca_nome', "$busca%", PDO::PARAM_STR);
     }
 } else {
     $sql = "SELECT * FROM procedimento ORDER BY nome ASC";
